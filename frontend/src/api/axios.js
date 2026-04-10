08f43ab89849483b6f45254e5ca2386b1192998d
Prashasti03 <dhanorkarprashasti@gmail.com>
2026-04-10 12:20:22 +0530

    Solving authorization error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codeb-internal-management-system.onrender.com/api"
+  baseURL: "https://codeb-internal-management-system.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
+  console.log("TOKEN:", localStorage.getItem("token"));
+  console.log("HEADERS BEFORE:", config.headers);
+
   const token = localStorage.getItem("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  console.log("HEADERS AFTER:", config.headers);
 
   return config;
 });
